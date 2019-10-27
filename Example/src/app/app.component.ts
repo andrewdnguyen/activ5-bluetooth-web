0d9a251e7ba0400d81311dd0f34bcce91e13ec2d
Sasri Dedigama <sasridedigama97@gmail.com>
2019-10-27 08:30:15 -0700

    I NEED TO SLEEP SO BAD

--- Example/src/app/app.component.ts
+++ Example/src/app/app.component.ts
@@ -13,7 +13,6 @@ export class AppComponent {
   public devices: A5Device[] = [];
   public isomData: string[] = [];
   public timeClicked1: number[] = [];
-  public timeClicked2: number[] = [];
   public pressureList: number[] = [];
 
   public deviceOneIsEvergreenMode: boolean;
@@ -24,33 +23,42 @@ export class AppComponent {
 
   public record = false;
   public displayResult1 = false;
+  public practiceStuff = false;
+
 
   public sensitivityValue = 0;
-  public timeBetweenBeats = 500;
+  public timeBetweenBeats = 500/0.5;
   public results = -1;
   public compressionAverage = -1;
 
   formatLabel(value: number) {
     return value;
   }
+  
+  
+  showPractice(){
+    this.practiceStuff = true
+    
+  }
 
   showResults1(){
     let total = 0; 
-    console.log(this.timeClicked1);
+    console.log('timeClicked1:', this.timeClicked1);
     for(let i = 1; i < this.timeClicked1.length-1; i++){
       console.log(this.timeClicked1[i]);
       let difference = this.timeClicked1[i] - this.timeClicked1[i-1];
       total = total + difference;
-      //console.log(total);
+      console.log(total);
     }
     let totalCompression = 0;
+    console.log(this.timeClicked1);
     for(let x = 0; x < this.pressureList.length; x++){
       totalCompression = totalCompression + this.pressureList[x];
     }
     let compAverage = totalCompression / this.pressureList.length;
-    this.compressionAverage = compAverage;
+    this.compressionAverage =  Math.trunc(compAverage);
     let average = total / this.timeClicked1.length;
-    this.results = average;
+    this.results = Math.trunc(60/((average/1000)));
     this.displayResult1 = true
   }
 
@@ -73,14 +81,6 @@ export class AppComponent {
 
   }
 
-  public registerTwo() {
-    console.log("Clicked button 2!");
-    let date2 = new Date();
-    let time2 = date2.getTime();
-    if(this.record === true){
-      this.timeClicked2.push(time2);
-    }
-  }
 
   public updateSensitivity(event: any) {
     this.sensitivityValue = event.value;
@@ -100,8 +100,6 @@ export class AppComponent {
 
       let currentInt1 = 0;
       let lastInt1 = 0;
-      let currentInt2 = 0;
-      let lastInt2 = 0;
       this.devices[index].getIsometricData().subscribe((data: string) => {
         this.isomData[index] = data;
         lastInt1 = currentInt1
@@ -109,19 +107,18 @@ export class AppComponent {
         let difference1 = currentInt1 - lastInt1;
         if(difference1 > (100 - this.sensitivityValue)){
           //if change is large enough, record pressure
-          //this.pressureList.push(currentInt1);
-          //this.currentIso = currentInt1;
+          this.pressureList.push(currentInt1);
+          this.currentIso = currentInt1;
           //Use this.currentIso to determine the background color could be if statements
+          if (this.currentIso > 556)
           console.log(this.sensitivityValue);
           this.registerOne();
-        }
-        lastInt2 = currentInt2
-        currentInt2 = parseInt(this.isomData[1]);
-        let difference2 = currentInt2 - lastInt2;
-        if(difference2 > (100 - this.sensitivityValue)) { 
-          this.registerTwo();
-        }
+        } 
       });
+      
+      
+      
+      
 
       this.devices[index].onDisconnect().subscribe((event: Event) => {
         this.devices[index] = undefined;
